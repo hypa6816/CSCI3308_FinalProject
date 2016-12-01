c6a6d5df8b3af960d1cc77e5d347f8402e00eb83
Igor Banaszuk <igba2150@colorado.edu>
2016-11-30 18:20:45 -0700

    Update results_pg.php

--- RateMyCourse/results_pg.php
+++ RateMyCourse/results_pg.php
@@ -15,14 +15,21 @@
           <?php
             $SearchedCourseTitle = $_GET['SearchQuery']; // get the SearchQuery from courseHero.php 
             
-            $db = mysql_connect('104.198.161.89', 'prototypeuser', 'flatiron'); //connnect to our mysql base
+            //use EXPLODE to parse the JSON string so users can search by acronym
+            list($prefix,$suffix) = explode(" ","$SearchedCourseTitle ");
+           
+            
+        $db = mysql_connect('104.198.161.89', 'prototypeuser', 'flatiron'); //connnect to our mysql base
             if(!$db){
                 echo "<p> Failed to connect to DB server </p>"; //error message for a failed connetion
                 die();
             }
             $my_db = mysql_select_db('RateMyCourse'); //select our data base
             //TODO: Make sure this is the best way to look for courses or implement more search methods
-            $search_query = "select distinct CourseTitle from CourseName where CourseTitle like '$SearchedCourseTitle%' order by CourseTitle" ; // mysql query 
+            $search_query = "select distinct CourseTitle from CourseName where CourseTitle like '%$SearchedCourseTitle%' or (subject like '$prefix' or course like '$suffix') order by CourseTitle" ; // mysql query 
+        
+       
+        
             $result = mysql_query($search_query) or die($search_query."<br/><br/>".mysql_error()); // send the query to our mysql DB. post error code if query fails     
             while($row=mysql_fetch_assoc($result)) // for the amount of rows the query returns echo the CourseTitle as a hyperlink to our content_pg.php
             { 
